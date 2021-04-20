2a62d0f32d0540dfd5845c43afc018b3ee964979
anthony-ashby <anthony.ashby@ymail.com>
2021-04-19 19:34:20 -0600

    Implemented Stream List.

--- src/components/ActiveStreams.js
+++ src/components/ActiveStreams.js
@@ -1,8 +1,45 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import api from "./api";
+import { useState, useEffect } from "react";
+import { Row, Col } from "reactstrap";
+import { makeStyles } from "@material-ui/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import VisibilityIcon from "@material-ui/icons/Visibility";
+
+const useStyles = makeStyles({
+  root: {
+    color: "#71ccdf",
+    textAlign: "left",
+  },
+  customLink: {
+    paddingTop: 2,
+    paddingBottom: 2,
+    color: "#71ccdf",
+    "&:hover": {
+      backgroundColor: "#71ccdf",
+      color: "#a44e62",
+    },
+  },
+});
+
+function ListItemLink(props) {
+  const classes = useStyles();
+  return (
+    <ListItem
+      button
+      component="a"
+      {...props}
+      target="_blank"
+      className={classes.customLink}
+    />
+  );
+}
 
 function ActiveStreams() {
+  const classes = useStyles();
   const [activeStreams, setActiveStreams] = useState([]);
 
   useEffect(() => {
@@ -18,9 +55,22 @@ function ActiveStreams() {
 
   return (
     <div>
-      {activeStreams.map((stream) => (
-        <div key={stream._id}>{stream.channel.display_name}</div>
-      ))}
+      <List component="nav" aria-label="main mailbox folders">
+        {activeStreams.map((stream) => (
+          <ListItemLink
+            key={stream._id}
+            className={classes.root}
+            href={stream.channel.url}
+          >
+            <ListItemText primary={stream.channel.display_name} />
+            <ListItemText
+              primary={stream.viewers}
+              style={{ textAlign: "right", paddingRight: 10 }}
+            />
+            <VisibilityIcon fontSize="small" />
+          </ListItemLink>
+        ))}
+      </List>
     </div>
   );
 }
